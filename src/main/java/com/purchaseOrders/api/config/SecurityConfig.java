e33be58e040967c8db0d729c131bad65169f5884
Miriã Fassarella <miriafassarella@gmail.com>
2024-01-25 06:56:40 -0500

    Add class config security

---
+++ src/main/java/com/purchaseOrders/api/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.purchaseOrders.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+	
+	
+    public void configureGlobal(AuthenticationManagerBuilder auth) 
+      throws Exception {
+        auth.inMemoryAuthentication().withUser("user")
+          .password("123").roles("USER");
+    }
+
+    
+	
+	
+}
