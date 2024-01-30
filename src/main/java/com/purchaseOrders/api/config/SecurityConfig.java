262f40af40799e6bd3df3953c898399285c16024
Miriã Fassarella <miriafassarella@gmail.com>
2024-01-30 07:03:51 -0500

    cookies

--- src/main/java/com/purchaseOrders/api/config/SecurityConfig.java
+++ src/main/java/com/purchaseOrders/api/config/SecurityConfig.java
@@ -1,21 +1,26 @@
 package com.purchaseOrders.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
 	
-	
-    public void configureGlobal(AuthenticationManagerBuilder auth) 
-      throws Exception {
-        auth.inMemoryAuthentication().withUser("user")
-          .password("123").roles("USER");
-    }
+	@Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+	      .anyRequest().authenticated()
+	      .and().httpBasic()
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+             return http.build();
+     }
 
-   }
+	
+ }
